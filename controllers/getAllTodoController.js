4d830936691f31407273f75fbce658dd9d92cdb0
AleksiKlimov <kuzmenkoaleksej59@gmail.com>
2022-11-26 02:19:32 +0300

    implement filtr on back

--- controllers/getAllTodoController.js
+++ controllers/getAllTodoController.js
@@ -1,10 +1,18 @@
 const Todo = require("../todoModel");
 
 const getAllTodos  = async (req, res)=> {
-
+  console.log(req.params.filter)
   try{
-    const todos = await Todo.find({});
+    const filter = req.params.filter
+    if(filter === 'all'){
+      const todos = await Todo.find({});
+      res.json(todos);
+      return;
+    }
+    const flag = filter === 'completed';
+    const todos = await Todo.find({completed: flag});
     res.json(todos);
+    return;
   }
   catch(err){
     console.log(err);
